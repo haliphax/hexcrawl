e4d29589f4616c200c0b665ef15bb4d80bc5f91e
haliphax <haliphax@gmail.com>
2023-09-27 10:30:38 -0500

    ✨ hexagon map plugin

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,41 +1,72 @@
 import Phaser from "phaser";
+import BoardPlugin from "phaser3-rex-plugins/plugins/board-plugin";
+import GetHexagonMap from "phaser3-rex-plugins/plugins/board/hexagonmap/GetHexagonMap";
+import { TileXYType } from "phaser3-rex-plugins/plugins/board/types/Position";
 
-class Example extends Phaser.Scene {
-	preload() {
-		this.load.setBaseURL("https://labs.phaser.io");
-		this.load.image("sky", "assets/skies/space3.png");
-		this.load.image("logo", "assets/sprites/phaser3-logo.png");
-		this.load.image("red", "assets/particles/red.png");
-	}
+class Main extends Phaser.Scene {
+	rexBoard!: BoardPlugin;
 
 	create() {
-		this.add.image(400, 300, "sky");
-
-		const particles = this.add.particles(0, 0, "red", {
-			speed: 100,
-			scale: { start: 1, end: 0 },
-			blendMode: "ADD",
+		const cellSize = 32;
+		const gridOffset = 8;
+		const board = this.rexBoard.add.board({
+			grid: {
+				gridType: "hexagonGrid",
+				size: cellSize,
+				staggeraxis: "y",
+				staggerindex: "odd",
+				x: cellSize + gridOffset,
+				y: cellSize + gridOffset,
+			},
 		});
-		const logo = this.physics.add.image(400, 100, "logo");
 
-		logo.setVelocity(100, 200);
-		logo.setBounce(1, 1);
-		logo.setCollideWorldBounds(true);
-		particles.startFollow(logo);
+		const tilemap = board.fit(
+			GetHexagonMap(board, 4),
+		) as unknown as TileXYType[];
+
+		const graphics = this.add.graphics({
+			lineStyle: {
+				width: 1,
+				color: 0x555555,
+				alpha: 1,
+			},
+		});
+
+		for (const tileXY of tilemap) {
+			const worldXY = board.tileXYToWorldXY(tileXY.x, tileXY.y);
+
+			graphics.strokePoints(
+				board.getGridPoints(tileXY.x, tileXY.y, true),
+				true,
+			);
+			(board.scene as Phaser.Scene).add
+				.text(worldXY.x, worldXY.y, `${tileXY.x},${tileXY.y}`, {
+					color: "#0c0",
+					fontSize: 14,
+				})
+				.setOrigin(0.5);
+		}
 	}
 }
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	scene: Example,
-	physics: {
-		default: "arcade",
-		arcade: {
-			gravity: { y: 200 },
-		},
+	height: 576,
+	plugins: {
+		scene: [
+			{
+				key: "rexBoard",
+				mapping: "rexBoard",
+				plugin: BoardPlugin,
+			},
+		],
 	},
+	scale: {
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		mode: Phaser.Scale.FIT,
+	},
+	scene: Main,
+	type: Phaser.AUTO,
+	width: 1024,
 };
 
 new Phaser.Game(config);
