b737f22037d942fbef51a34ef67352a624408d74
haliphax <haliphax@gmail.com>
2023-09-27 18:58:35 -0500

    ✨ double tap/long press to control zoom on mobile

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,1 +1,7 @@
+export const DOUBLE_TAP_DELAY_MS = 500;
+export const LONG_PRESS_DELAY_MS = 750;
 export const TILE_SIZE = 72;
+export const ZOOM_FACTOR = 0.1;
+export const ZOOM_FACTOR_TAP = 0.25;
+export const ZOOM_MIN = 0.25;
+export const ZOOM_MAX = 4;
--- src/client/scenes/hexmap.ts
+++ src/client/scenes/hexmap.ts
@@ -1,4 +1,13 @@
-import { TILE_SIZE } from "../constants";
+import { Cameras } from "phaser";
+import {
+	DOUBLE_TAP_DELAY_MS,
+	LONG_PRESS_DELAY_MS,
+	TILE_SIZE,
+	ZOOM_FACTOR,
+	ZOOM_FACTOR_TAP,
+	ZOOM_MAX,
+	ZOOM_MIN,
+} from "../constants";
 import terrainSprite from "../images/spritesheet.png";
 import mapFile from "../maps/map.json";
 
@@ -7,12 +16,24 @@ export default class HexMap extends Phaser.Scene {
 		super("scene-main");
 	}
 
+	private clampZoom(
+		cam: Cameras.Scene2D.Camera,
+		zoomDir: -1 | 0 | 1,
+		tap = false,
+	) {
+		const factor = tap ? ZOOM_FACTOR_TAP : ZOOM_FACTOR;
+		const dist = zoomDir < 0 ? 1 - factor : 1 + factor;
+		cam.setZoom(Math.max(ZOOM_MIN, Math.min(ZOOM_MAX, cam.zoom * dist)));
+	}
+
 	preload() {
 		this.load.image("terrain", terrainSprite);
 		this.load.tilemapTiledJSON("map", mapFile);
 	}
 
 	create() {
+		// --- load map ---
+
 		const tilemap = this.make.tilemap({ key: "map" });
 		const tileset = tilemap.addTilesetImage(
 			"terrain_tiles",
@@ -25,30 +46,75 @@ export default class HexMap extends Phaser.Scene {
 		layer.cullCallback = () =>
 			layer.getTilesWithin(0, 0, tilemap.width, tilemap.height);
 
-		this.input.addListener("wheel", (ev: WheelEvent) => {
-			const zoomDir = ev.deltaY > 0 ? -1 : ev.deltaY < 0 ? 1 : 0;
-
-			this.cameras.main.setZoom(
-				Math.max(0.1, Math.min(2, this.cameras.main.zoom + zoomDir * 0.1)),
-			);
-		});
-
-		this.cameras.main
-			.setOrigin(0.5, 0.5)
-			.setBounds(
-				TILE_SIZE / 4,
-				TILE_SIZE / 2,
-				tilemap.width * (TILE_SIZE * 0.75) - TILE_SIZE,
-				tilemap.height * TILE_SIZE - TILE_SIZE / 2,
-			);
+		// --- camera and controls ---
 
 		const cam = this.cameras.main;
 
-		this.input.on("pointermove", (p: Phaser.Input.Pointer) => {
-			if (!p.isDown) return;
+		cam.setOrigin(0.5, 0.5).setBounds(
+			// account for horizontal hex gap
+			TILE_SIZE / 4,
+			// account for vertical hex gap
+			TILE_SIZE / 2,
+			// account for tile width overlap
+			tilemap.width * (TILE_SIZE * 0.75) - TILE_SIZE,
+			// account for tile height overlap
+			tilemap.height * TILE_SIZE - TILE_SIZE / 2,
+		);
 
+		// scroll
+		this.input.addListener("wheel", (ev: WheelEvent) => {
+			const zoomDir = ev.deltaY > 0 ? -1 : ev.deltaY < 0 ? 1 : 0;
+			this.clampZoom(cam, zoomDir);
+		});
+
+		let tapTime = 0;
+		let longPressTimer: NodeJS.Timeout;
+		let wasLongPress = false;
+
+		const longPressHandler = () => {
+			wasLongPress = true;
+			this.clampZoom(cam, -1, true);
+		};
+
+		// drag
+		this.input.on("pointermove", (p: Phaser.Input.Pointer) => {
+			if (!p.isDown) {
+				return;
+			}
+
+			if (p.getDistance() < 24) {
+				return;
+			}
+
+			clearTimeout(longPressTimer);
 			cam.scrollX -= (p.x - p.prevPosition.x) / cam.zoom;
 			cam.scrollY -= (p.y - p.prevPosition.y) / cam.zoom;
+		});
+
+		this.input.on("pointerdown", () => {
+			wasLongPress = false;
+			longPressTimer = setTimeout(longPressHandler, LONG_PRESS_DELAY_MS);
+		});
+
+		// tap/click
+		this.input.on("pointerup", (p: Phaser.Input.Pointer) => {
+			clearTimeout(longPressTimer);
+
+			// double tap
+			if (tapTime > 0 && p.time - tapTime < DOUBLE_TAP_DELAY_MS) {
+				tapTime = 0;
+				this.clampZoom(cam, 1, true);
+				return;
+			}
+
+			// long press; discard
+			if (wasLongPress) {
+				tapTime = 0;
+				return;
+			}
+
+			// single tap
+			tapTime = p.time;
 		});
 	}
 }
