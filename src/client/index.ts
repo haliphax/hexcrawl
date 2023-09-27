7f308dda9256700477dc86c0b6b280cb7803722e
haliphax <haliphax@gmail.com>
2023-09-27 16:08:40 -0500

    ✨ use tiled map editor

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,72 +1,49 @@
 import Phaser from "phaser";
-import BoardPlugin from "phaser3-rex-plugins/plugins/board-plugin";
-import GetHexagonMap from "phaser3-rex-plugins/plugins/board/hexagonmap/GetHexagonMap";
-import { TileXYType } from "phaser3-rex-plugins/plugins/board/types/Position";
+import terrainSprite from "./images/spritesheet.png";
+import mapFile from "./maps/map.json";
 
+const TILE_SIZE = 72;
+
+// tiles
 class Main extends Phaser.Scene {
-	rexBoard!: BoardPlugin;
+	constructor() {
+		super("scene-main");
+	}
+
+	preload() {
+		this.load.image("terrain", terrainSprite);
+		this.load.tilemapTiledJSON("map", mapFile);
+	}
 
 	create() {
-		const cellSize = 32;
-		const gridOffset = 8;
-		const board = this.rexBoard.add.board({
-			grid: {
-				gridType: "hexagonGrid",
-				size: cellSize,
-				staggeraxis: "y",
-				staggerindex: "odd",
-				x: cellSize + gridOffset,
-				y: cellSize + gridOffset,
-			},
-		});
+		const tilemap = this.make.tilemap({ key: "map" });
+		const tileset = tilemap.addTilesetImage(
+			"terrain_tiles",
+			"terrain",
+			TILE_SIZE,
+			TILE_SIZE,
+		)!;
+		const layer = tilemap.createLayer(
+			"Terrain",
+			tileset,
+			-TILE_SIZE,
+			-TILE_SIZE,
+		)!;
 
-		const tilemap = board.fit(
-			GetHexagonMap(board, 4),
-		) as unknown as TileXYType[];
-
-		const graphics = this.add.graphics({
-			lineStyle: {
-				width: 1,
-				color: 0x555555,
-				alpha: 1,
-			},
-		});
-
-		for (const tileXY of tilemap) {
-			const worldXY = board.tileXYToWorldXY(tileXY.x, tileXY.y);
-
-			graphics.strokePoints(
-				board.getGridPoints(tileXY.x, tileXY.y, true),
-				true,
-			);
-			(board.scene as Phaser.Scene).add
-				.text(worldXY.x, worldXY.y, `${tileXY.x},${tileXY.y}`, {
-					color: "#0c0",
-					fontSize: 14,
-				})
-				.setOrigin(0.5);
-		}
+		layer.cullCallback = () =>
+			layer.getTilesWithin(0, 0, tilemap.width, tilemap.height);
 	}
 }
 
 const config: Phaser.Types.Core.GameConfig = {
-	height: 576,
-	plugins: {
-		scene: [
-			{
-				key: "rexBoard",
-				mapping: "rexBoard",
-				plugin: BoardPlugin,
-			},
-		],
-	},
+	height: 720,
 	scale: {
 		autoCenter: Phaser.Scale.CENTER_BOTH,
 		mode: Phaser.Scale.FIT,
 	},
-	scene: Main,
+	scene: [Main],
 	type: Phaser.AUTO,
-	width: 1024,
+	width: 1280,
 };
 
 new Phaser.Game(config);
