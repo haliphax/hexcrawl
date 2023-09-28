56c499020e55fcab8d9269626aab5d239e1e6a24
haliphax <haliphax@gmail.com>
2023-09-27 19:36:02 -0500

    ➖ remove unused packages

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,4 @@
 import compression from "compression";
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import { createHttpTerminator } from "http-terminator";
@@ -14,7 +13,6 @@ if (process.env.NODE_ENV !== "production") {
 
 app.use(compression());
 app.use(express.json({ limit: "10mb" }));
-app.use(cookieParser());
 app.use(express.static("dist"));
 
 app.disable("x-powered-by");
