0c641a4b0568bdcad708c2c89f2b834f3394fb44
haliphax <haliphax@gmail.com>
2025-05-28 12:43:03 -0500

    🧑‍💻 pre-commit -> husky

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,7 +1,10 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	build: {
+		emptyOutDir: true,
+		outDir: path.join(__dirname, "..", "dist"),
 		target: "esnext",
 	},
 });
